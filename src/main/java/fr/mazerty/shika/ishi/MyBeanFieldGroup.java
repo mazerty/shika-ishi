8fe6707e5366d6dfb796aaf2a13b9a413d3fafa7
Mazerty <mazerty@gmail.com>
2016-02-20 18:52:45 +0100

    déplacement des composants vaadin personnalisés dans shika-ishi

---
+++ src/main/java/fr/mazerty/shika/ishi/MyBeanFieldGroup.java
@@ -0,0 +1,21 @@
+package fr.mazerty.shika.ishi;
+
+import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import javaslang.control.Try;
+
+public class MyBeanFieldGroup<T> extends BeanFieldGroup<T> {
+
+    public MyBeanFieldGroup(Class<T> beanType) {
+        super(beanType);
+        setBuffered(false);
+
+        Try.of(beanType::newInstance)
+                .onFailure(Throwable::printStackTrace) // TODO
+                .andThen(this::setItemDataSource);
+    }
+
+    public T getBean() {
+        return getItemDataSource().getBean();
+    }
+
+}
---
+++ src/main/java/fr/mazerty/shika/ishi/MyPasswordField.java
@@ -0,0 +1,11 @@
+package fr.mazerty.shika.ishi;
+
+import com.vaadin.ui.PasswordField;
+
+public class MyPasswordField extends PasswordField {
+
+    public MyPasswordField() {
+        setNullRepresentation("");
+    }
+
+}
