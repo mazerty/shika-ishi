c508418e8a53d37fdd418ef22b7f26c69cd97771
Mazerty <mazerty@gmail.com>
2016-03-18 19:41:21 +0100

    adaptation du code pour utiliser les classes jooq générées

--- src/main/java/fr/mazerty/shika/ishi/dao/UserDaoImpl.java
+++ src/main/java/fr/mazerty/shika/ishi/dao/UserDaoImpl.java
@@ -1,25 +1,27 @@
 package fr.mazerty.shika.ishi.dao;
 
 import fr.mazerty.shika.ishi.bean.User;
+import fr.mazerty.shika.ishi.dao.jooq.Tables;
+import fr.mazerty.shika.ishi.dao.jooq.tables.TApplication;
+import fr.mazerty.shika.ishi.dao.jooq.tables.TUser;
+import fr.mazerty.shika.ishi.dao.jooq.tables.TUserApplication;
 
 import javax.enterprise.context.ApplicationScoped;
-
-import static org.jooq.impl.DSL.field;
-import static org.jooq.impl.DSL.table;
 
 @ApplicationScoped
 public class UserDaoImpl extends MyDao implements UserDao {
 
     @Override
     public User selectByEmailAndApplication(User user, String application) {
-        return dslContext.select()
-                .from(table("torii.t_user").as("u"))
-                .innerJoin(table("torii.t_user_application").as("ua"))
-                .on(field("u.id").equal(field("ua.user_fk")))
-                .innerJoin(table("torii.t_application").as("a"))
-                .on(field("ua.application_fk").equal(field("a.id")))
-                .where(field("u.email").equal(user.getEmail()))
-                .and(field("a.name").equal(application))
+        TUser u = Tables.T_USER;
+        TUserApplication ua = Tables.T_USER_APPLICATION;
+        TApplication a = Tables.T_APPLICATION;
+
+        return dslContext.select().from(u)
+                .innerJoin(ua).on(ua.USER_FK.equal(u.ID))
+                .innerJoin(a).on(a.ID.equal(ua.APPLICATION_FK))
+                .where(u.EMAIL.equal(user.getEmail()))
+                .and(a.NAME.equal(application))
                 .fetchAnyInto(User.class);
     }
 
