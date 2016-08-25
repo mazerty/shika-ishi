c878756c6f7e9b190bf79c6f35acf2080d3c81c0
Mazerty <mazerty@gmail.com>
2016-08-25 16:46:04 +0000

    simplification du deployment arquillian en utilisant mavenimporter

--- src/main/java/fr/mazerty/shika/ishi/test/MyDaoTest.java
+++ src/main/java/fr/mazerty/shika/ishi/test/MyDaoTest.java
@@ -4,34 +4,27 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
+import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
+import org.jboss.shrinkwrap.resolver.api.maven.strategy.AcceptAllStrategy;
 import org.jooq.DSLContext;
 import org.jooq.Table;
 import org.jooq.TableRecord;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
-import java.io.File;
 
 @RunWith(Arquillian.class)
 public abstract class MyDaoTest {
-
-    public static final String WEB_INF = "src/main/webapp/WEB-INF/";
 
     @Inject
     private DSLContext dslContext;
 
     @Deployment
     public static WebArchive deployment() {
-        PomEquippedResolveStage pers = Maven.resolver().loadPomFromFile("pom.xml");
-
-        return ShrinkWrap.create(WebArchive.class)
-                .addPackages(true, "fr.mazerty.shika")
-                .addAsWebInfResource(new File(WEB_INF + "beans.xml"))
-                .addAsWebInfResource(new File(WEB_INF + "web.xml"))
-                .addAsLibraries(pers.resolve("org.jooq:jooq").withTransitivity().asFile())
-                .addAsLibraries(pers.resolve("org.assertj:assertj-core").withTransitivity().asFile());
+        return ShrinkWrap.create(MavenImporter.class)
+                .loadPomFromFile("pom.xml")
+                .importBuildOutput(AcceptAllStrategy.INSTANCE)
+                .as(WebArchive.class);
     }
 
     protected void delete(Table... tables) {
