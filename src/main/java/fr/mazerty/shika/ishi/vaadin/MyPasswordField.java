a18e5f2ac943010f9b4b6a6fb5119f757a3105bc
mazerty <mazerty@gmail.com>
2017-01-14 17:10:28 +0000

    custom password field with a decent null representation

---
+++ src/main/java/fr/mazerty/shika/ishi/vaadin/MyPasswordField.java
@@ -0,0 +1,11 @@
+package fr.mazerty.shika.ishi.vaadin;
+
+import com.vaadin.ui.PasswordField;
+
+public class MyPasswordField extends PasswordField {
+
+    public MyPasswordField() {
+        setNullRepresentation("");
+    }
+
+}
