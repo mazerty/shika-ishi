4e4812de79e04525d643c35166ead60e01b1795a
Mazerty <mazerty@gmail.com>
2017-01-21 16:33:22 +0000

    fullsizing a vertical layout is a bad idea after all (for screen resizing for example)

--- src/main/java/fr/mazerty/shika/ishi/vaadin/MyView.java
+++ src/main/java/fr/mazerty/shika/ishi/vaadin/MyView.java
@@ -4,9 +4,4 @@ import com.vaadin.navigator.View;
 import com.vaadin.ui.VerticalLayout;
 
 public abstract class MyView extends VerticalLayout implements View, UiManager {
-
-    protected MyView() {
-        setSizeFull();
-    }
-
 }
