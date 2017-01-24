d83418210db033418a88b2d5392b985b69d83b2a
Mazerty <mazerty@gmail.com>
2017-01-24 12:14:16 +0000

    myview.enter() is now optional and can be overridden if needed

--- src/main/java/fr/mazerty/shika/ishi/vaadin/MyView.java
+++ src/main/java/fr/mazerty/shika/ishi/vaadin/MyView.java
@@ -1,7 +1,16 @@
 package fr.mazerty.shika.ishi.vaadin;
 
 import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.VerticalLayout;
 
 public abstract class MyView extends VerticalLayout implements View, UiManager {
+
+    /**
+     * Override if some code has to be executed upon showing the view.
+     */
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+    }
+
 }
