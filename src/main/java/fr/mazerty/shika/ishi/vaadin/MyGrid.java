bd734415083c24311c378c48133e3c2e98aeb07a
Mazerty <mazerty@gmail.com>
2017-02-02 20:34:28 +0000

    adds handy setColumnHeaderCaptions method

--- src/main/java/fr/mazerty/shika/ishi/vaadin/MyGrid.java
+++ src/main/java/fr/mazerty/shika/ishi/vaadin/MyGrid.java
@@ -3,6 +3,8 @@ package fr.mazerty.shika.ishi.vaadin;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.ui.Grid;
 
+import java.util.stream.IntStream;
+
 /**
  * Custom {@link Grid} with integrated {@link BeanItemContainer}
  */
@@ -10,6 +12,14 @@ public class MyGrid<T> extends Grid {
 
     public MyGrid(Class<T> beanType) {
         super(new BeanItemContainer<>(beanType));
+    }
+
+    /**
+     * Sets the header captions of visible columns in order of appearance
+     */
+    public void setColumnHeaderCaptions(String... captions) {
+        IntStream.range(0, captions.length)
+                .forEach(i -> getColumns().get(i).setHeaderCaption(captions[i]));
     }
 
 }
