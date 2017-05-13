f29e8689e7ebb339772001f6726629f70fadc6a3
Mazerty <mazerty@gmail.com>
2017-05-13 16:51:34 +0000

    apparently mybutton is useless

--- src/main/java/fr/mazerty/shika/ishi/vaadin/PrimaryButton.java
+++ src/main/java/fr/mazerty/shika/ishi/vaadin/PrimaryButton.java
@@ -1,12 +1,14 @@
 package fr.mazerty.shika.ishi.vaadin;
 
+import com.vaadin.ui.Button;
+
 import static com.vaadin.event.ShortcutAction.KeyCode.ENTER;
 import static com.vaadin.ui.themes.ValoTheme.BUTTON_PRIMARY;
 
 /**
- * Custom {@link MyButton} with "primary" style and behavior
+ * Custom {@link Button} with "primary" style and behavior
  */
-public class PrimaryButton extends MyButton {
+public class PrimaryButton extends Button {
 
     public PrimaryButton() {
         addStyleName(BUTTON_PRIMARY);
